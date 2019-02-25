e2e0eb66e459c60d68d31b88ec493a65a4728ced
DrewE <andrew.engelstein@gmail.com>
2019-02-25 10:48:33 -0800

    Initial commit with W7D5 work

---
+++ BenchBnB/app/models/user.rb
@@ -0,0 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  attr_reader :password
+  
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+  
+  def password=(pw)
+    @password = pw;
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def self.find_by_credentials(un, pw)
+    user = User.find_by(username: un)
+    # return nil unless user
+    (user && user.is_password?(pw)) ? user : nil
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+  
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+  
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+end
